d25dca85ebd2302ca8d9e608b8b08ee85f8e0067
Leonid Lavrov <274852@gmail.com>
2023-05-26 22:22:34 +0300

    Using animations in screen transitions

--- app/src/main/java/com/sumin/composeanimations/ui/screen/AnimateContent.kt
+++ app/src/main/java/com/sumin/composeanimations/ui/screen/AnimateContent.kt
@@ -1,5 +1,15 @@
 package com.sumin.composeanimations.ui.screen
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.with
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -7,8 +17,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnimateContent() {
 
@@ -27,10 +39,22 @@ fun AnimateContent() {
         ) {
             Text(text = "Switch screens")
         }
-        if (isFirstScreenLaunched) {
-            Screen1()
-        } else {
-            Screen2()
+
+        AnimatedContent(
+            targetState = isFirstScreenLaunched,
+            transitionSpec = {
+                if (targetState) {
+                    slideInHorizontally(tween(2000)) { -it } with slideOutHorizontally(tween(2000)) { it }
+                } else {
+                    slideInHorizontally(tween(2000)) { it } with slideOutHorizontally(tween(2000)) { -it }
+                }
+            }
+        ) {
+            if (it) {
+                Screen1()
+            } else {
+                Screen2()
+            }
         }
     }
 }
